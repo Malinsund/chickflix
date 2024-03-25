912b791c3f78430fb810ddcb745f52f7d744cfd4
Malin Sundberg <143407879+Malinsund@users.noreply.github.com>
2024-03-25 14:59:01 +0100

    mapped the movie thumbnails, added remote patterns in the next.config
    Co-authored-by: Annamedie <144950259+Annamedie@users.noreply.github.com>
    Co-authored-by: sallymaria99 <144012169+sallymaria99@users.noreply.github.com>
    Co-authored-by: elsabonde <elsasofiabonde@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+    ],
+  },
   experimental: {
     typedRoutes: true,
   },

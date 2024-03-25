4c4418e5e45c52a547fa1d9bf7e1ba9582278735
Malin Sundberg <143407879+Malinsund@users.noreply.github.com>
2024-03-25 13:39:55 +0100

    added the global.css file and tested it

--- app/UI/header.tsx
+++ app/UI/header.tsx
@@ -2,7 +2,7 @@ import { BookmarkIcon } from "@heroicons/react/24/outline";
 
 export default function Header() {
   return (
-    <header className="bg-[#282525] flex flex-row justify-between">
+    <header className="bg-[#282525] flex justify-between">
       <h1 className="text-blue-800">Chick Flix</h1>
       {/* <span>💜</span> */}
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./global.css";
+
 import Header from "./UI/header";
 
 const inter = Inter({ subsets: ["latin"] });

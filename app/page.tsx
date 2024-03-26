24bb8c24c65cc79a7c997fced99f850ad0946267
Elsa Bonde <elsasofiabonde@gmail.com>
2024-03-26 10:09:16 +0100

    Some styling for movies on startpage that matched the Figma sketches. Also added year, rating and bookmark according to the Figma layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { BookmarkIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import movies from "../movies.json";
 
@@ -11,13 +12,23 @@ export default function Home() {
           className="bg-white bg-opacity-50 m-5 flex flex-col"
           key={movie.title}
         >
+          <h3 className="text-center p-3">{movie.title}</h3>
           <Image
             src={movie.thumbnail}
             height={100}
             width={100}
             alt={movie.title}
+            style={{ height: "100%", width: "auto", paddingRight: "20px", paddingLeft: "20px"}}
           ></Image>
-          <h3>{movie.title}</h3>
+          <div className="flex flex-row justify-between p-5">
+            <p>{movie.year}</p>
+            <div className="flex flex-row">
+              <p>{movie.rating}</p>
+              <p>
+                <BookmarkIcon className="text-white h-7 w-7" />
+              </p>
+            </div>
+          </div>
         </div>
       ))}
     </div>

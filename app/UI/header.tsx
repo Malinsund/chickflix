82d09832a45510345facefab60c9a113804151c9
sallymaria99 <144012169+sallymaria99@users.noreply.github.com>
2024-03-26 10:28:25 +0100

    Added the Link-tag around logo ChickFlix, for linking back Home.

--- app/UI/header.tsx
+++ app/UI/header.tsx
@@ -1,10 +1,12 @@
 import { BookmarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="bg-[#282525] flex justify-between p-4">
-      <h1 className="text-blue-400">Chick Flix</h1>
-
+      <Link href="/">
+        <h1 className="text-blue-400">Chick Flix</h1>
+      </Link>
       <BookmarkIcon className="text-white h-7 w-7" />
     </header>
   );
